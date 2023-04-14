876215bfa9b9647f261e36578efcc89b37458681
Leo <vuongvankhanh89@gmail.com>
2023-04-14 20:22:09 +0700

    clear linting problems

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,24 @@
+//  import { Strapi } from '@strapi/strapi'
+
 export default {
-  /**
+    /**
    * An asynchronous register function that runs before
    * your application is initialized.
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+    register(/*{ strapi } : { strapi: Strapi }*/) {
+        return
+    },
 
-  /**
+    /**
    * An asynchronous bootstrap function that runs before
    * your application gets started.
    *
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
-};
+    bootstrap(/*{ strapi } : { strapi: Strapi }*/) {
+        return
+    },
+}
